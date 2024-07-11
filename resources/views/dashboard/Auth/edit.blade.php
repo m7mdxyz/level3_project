c929317dc8f9115fd672b5824299dc3506c74de2
Mohammed <mouhmad11@gmail.com>
2024-07-11 22:15:18 +0300

    ...

---
+++ resources/views/dashboard/Auth/edit.blade.php
@@ -0,0 +1,60 @@
+@extends("dashboard.layout.app")
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">My Profile</h1>
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+<div class="row">
+    <!-- /.col-lg-4 -->
+</div>
+<!-- /.row -->
+
+<div class="row">
+        <div class="col-lg-5">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    User Profile #{{$user->id}}
+                </div>
+                <div class="panel-body">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ asset($user->profile_pic) }}" alt="Profile Picture" style="width: 100px; height: 100px;">
+                        <div class="card-body">
+                            <h5 class="card-title">Name: {{ $user->name }}</h5>
+                            <p>Email: {{ $user->email }}</p>
+                            <!-- <p>Blogs Count: {{ $blogsCount }}</p> -->
+                            <p>User Type: {{ $user->user_type_id == 1 ? 'Admin' : 'Writer' }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-footer">
+                    <!-- Panel Footer -->
+                    Blogs Count: {{ $blogsCount }}
+                </div>
+            </div>
+
+            <form action="/login" method="POST" role="form">
+                            @csrf
+                            @method('PATCH')
+                            <fieldset>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="name" name="name" type="text" autofocus value={{$user->name}}>
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="email" name="email" type="email" value={{$user->email}}>
+                                </div>
+                                
+                            </fieldset>
+                        </form>
+            <a href="/myprofile/edit" class="btn btn-primary">Save</a>
+            <a href="javascript:void(0);" onclick="window.history.back();" class="btn btn-primary">Back to Dashboard</a>
+        </div>
+        <!-- /.col-lg-4 -->
+
+</div>
+
+
+@endsection
